23e0275ae52948e3fc092ddcd20cca4ff6b7317c
mrsnobody16-beep <mrsnobody16@gmail.com>
2025-09-01 07:19:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Smooth scroll navigation
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change every 3 seconds
+}
